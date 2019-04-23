29d20ecb3a47aad55d6795d56d7d9187aaea35df
sowmyaneni <ssneni@gmail.com>
2019-04-22 17:44:46 -0700

    Implement Grocery list methods

--- app.js
+++ app.js
@@ -1,1 +1,88 @@
 // TODO
+
+
+
+// var App = () => (
+//     <div>
+//       <h2>My Todo List</h2>
+//       <TodoList />
+//     </div>
+//   );
+
+
+// var MyGroceries = (props) => {
+//     var onListItemClick = (event) => {
+//         alert("I got clicked");
+//     };
+//     return (
+//         <ul>
+//             <li onClick={onListItemClick}>{props.tobuy[0]}</li>
+//             <li onClick={onListItemClick}>{props.tobuy[1]}</li>
+//         </ul>
+//     );
+// };
+
+var GroceryList = () => (
+<div>
+    <h2> grocery list </h2>
+    <MyGroceries tobuy = {['Coffee', 'Tea']}/>
+</div>
+
+);
+
+
+// ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+class GroceryListItem extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            done: false,
+            hover: false
+        };
+    }
+    onListItemClick() {
+        this.setState({
+            done: !this.state.done
+        });
+    }
+
+    onMouseEnter(){
+        this.setState({
+            hover: true
+        })
+    }
+
+    onMouseLeave(){
+        this.setState({
+            hover: true
+        })
+    }
+
+
+    render(){
+        var style = {
+            textDecoration: this.state.done? 'line-through' : 'none',
+            fontWeight: this.state.hover? 'bold' : 'none'
+        };
+        return(
+            // <li onClick={this.props.onClick}>{this.props.item}</li>
+            <li style = {style} onMouseEnter={this.onMouseEnter.bind(this)} onMouseLeave={this.onMouseLeave.bind(this)} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+        );
+    }
+}
+
+var MyGroceries = (props) => {
+    var onListItemClick = (event) => {
+        alert("I got clicked");
+    };
+    return(
+    <ul>
+        {props.tobuy.map(item =>
+            <GroceryListItem item = {item} onClick={onListItemClick}/>
+             )}
+    </ul>
+    );
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
